42111d14db42213caaa79fd1afbeb43c8de8f594
Deng Guihui <guihui.deng@desaysv.com>
2023-06-25 21:08:11 +0800

    sync

--- gysohooks/m_addon.py
+++ gysohooks/m_addon.py
@@ -1,21 +1,22 @@
 import json
+import uuid
+
 from mitmproxy import ctx, http
-from datetime import datetime
-import aiohttp
+
+
+uuid_dict = {}
 
 
 class GysoAddon:
     def __init__(self, app_s, queue_s):
         self.app = app_s
         self.queue_m = queue_s
-        self.data = []
 
-    def response(self, flow: http.HTTPFlow) -> None:
+    def request(self, flow: http.HTTPFlow):
         request_time = flow.request.timestamp_start
-        response_time = flow.response.timestamp_end
-        time_diff = response_time - request_time
-
+        random_uuid = uuid.uuid4()
         request_info = {
+            'type': 'request',
             'url': flow.request.url,
             'method': flow.request.method,
             'headers': dict(flow.request.headers),
@@ -23,8 +24,18 @@ class GysoAddon:
             'raw': flow.request.get_text(),
             'timestamp': str(request_time),
         }
+        uuid_dict[str(request_time)] = random_uuid
+        req_package = json.dumps(request_info)
+        self.queue_m.put(req_package)
+
+    def response(self, flow: http.HTTPFlow) -> None:
+        request_time = flow.request.timestamp_start
+        response_time = flow.response.timestamp_end
+        time_diff = response_time - request_time
 
         response_info = {
+            'type': 'response',
+            'url': flow.request.url,
             'status_code': flow.response.status_code,
             'headers': dict(flow.response.headers),
             'content': flow.response.content.decode(),
@@ -33,12 +44,5 @@ class GysoAddon:
             'time_diff': str(time_diff),
         }
 
-        self.data = {
-            'request': request_info,
-            'response': response_info
-        }
-        req_package = json.dumps(self.data, indent=2)
-        self.queue_m.put(req_package)
-
-    def done(self):
-        print("done**********************************************")
+        res_package = json.dumps(response_info)
+        self.queue_m.put(res_package)
